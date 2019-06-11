b48869d146492360b3ab2e31f50d639f73e91b58
Harue Sado <amuamuknit@gmail.com>
2019-06-11 17:34:51 +0900

    css分離、whileで九九追加

--- multiplication.php
+++ multiplication.php
@@ -3,24 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>掛け算九九</title>
-<style>
-table { border-collapse: collapse;}
-th,td {
-    text-align: center;
-    vertical-align: center;
-    padding: .5em;
-    border: 1px solid #666;
-}
-.container {
-    display: flex;
-}
-.inner {
-    padding: 1em;
-}
-.even {
-    background: rgba(0,255,255,.2);
-}
-</style>
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
    <h1>掛け算九九</h1>
@@ -59,7 +43,7 @@ for($i = 1;$i < 10;$i++){
 ?>
                 </table>
             </div>
-            <div class="inner">
+            <div class="inner code">
                 <pre>
                     <code>
                     &lt;table&gt;
@@ -98,5 +82,81 @@ for($i = 1;$i < 10;$i++){
         </div>
     </section>
 <a href="/">戻る</a>
+<section>
+    <h2>whileで九九</h2>
+    <div class="container">
+        <div class="inner">
+                <table>
+                    <tr>
+                        <th>&nbsp;</th>
+                        <th>1</th>
+                        <th>2</th>
+                        <th>3</th>
+                        <th>4</th>
+                        <th>5</th>
+                        <th>6</th>
+                        <th>7</th>
+                        <th>8</th>
+                        <th>9</th>
+                    </tr>
+<?php
+$i = 1;
+while($i < 10){
+    echo "<tr>\n<th>".$i."</th>\n";
+    $l = 1;    
+    while($l <= 9){
+        if($i * $l % 2 == 0){
+            echo "<td class='even'>".$i * $l."</td>\n";
+        } else {
+            echo "<td>".$i * $l."</td>\n";
+        }
+        $l++;
+    }
+    echo "</tr>\n";
+    $i++;
+}
+?>
+            </table>
+        </div>
+        <div class="inner code">
+            <pre>
+                <code>
+                    &lt;table&gt;
+                        &lt;tr&gt;
+                            &lt;th&gt;&nbsp;&lt;/th&gt;
+                            &lt;th&gt;1&lt;/th&gt;
+                            &lt;th&gt;2&lt;/th&gt;
+                            &lt;th&gt;3&lt;/th&gt;
+                            &lt;th&gt;4&lt;/th&gt;
+                            &lt;th&gt;5&lt;/th&gt;
+                            &lt;th&gt;6&lt;/th&gt;
+                            &lt;th&gt;7&lt;/th&gt;
+                            &lt;th&gt;8&lt;/th&gt;
+                            &lt;th&gt;9&lt;/th&gt;
+                        &lt;/tr&gt;
+    &lt;?php
+    $i = 1;
+    while($i &lt; 10){
+        echo "&lt;tr&gt;\n&lt;th&gt;".$i."&lt;/th&gt;\n";
+        $l = 1;    
+        while($l &lt;= 9){
+            if($i * $l % 2 == 0){
+                echo "&lt;td class='even'&gt;".$i * $l."&lt;/td&gt;\n";
+            } else {
+                echo "&lt;td&gt;".$i * $l."&lt;/td&gt;\n";
+            }
+            $l++;
+        }
+        echo "&lt;/tr&gt;\n";
+        $i++;
+    }
+    ?&gt;
+                &lt;/table&gt;
+
+                </code>
+            </pre>
+        </div>
+    </div>
+</section>
 </body>
 </html>
